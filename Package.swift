ee26fce66ba889147920954551a47a70d0dbe360
SoyWeroDev <psvita.miner117@gmail.com>
2026-08-11 23:58:33 -0600

    Initial commit: Project structure, gitignore and env templates

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "DENUEExplorer",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "DENUEExplorer", targets: ["App"])
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [],
+            path: "Sources/App"
+        )
+    ]
+)
